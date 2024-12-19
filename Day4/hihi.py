93666e3dd915bd532ca39c1cd105182d7ddcf033
daehyeok1223 <143463184+daehyeok1223@users.noreply.github.com>
2024-12-19 16:12:49 +0900

    Delete Day4 directory

--- Day4/hihi.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def walk(self):
-        print("사람이 걷는다")
-class SuperMan(Person):
-    def fly(self):
-        print('난다')#상속으로 walk사용 가능
-    def walk(self):
-        print('슈퍼맨이 걷는다')#overriding(재정의)
